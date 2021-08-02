cef9e94e354c82a189ca4daf198d0f0e6cb0637a
Menna-AlHussein <48181642+Menna-AlHussein@users.noreply.github.com>
2021-08-02 12:29:08 +0200

    First

---
+++ src/Example.js
@@ -0,0 +1,18 @@
+import { Button } from 'bootstrap';
+import React from 'react';
+
+
+
+const Example = () => {
+
+    //new state var called count
+  //  const [count, setCount] = useState(0);
+
+    return (
+        <div>
+            <Button>Click Me</Button>
+        </div>
+    )
+}
+
+export default Example;
